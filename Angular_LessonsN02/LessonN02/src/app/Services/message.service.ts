303c6ecc771975cc5c0984d9dfa7257869057f57
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-05-25 13:13:28 +0500

    Add message service

---
+++ Angular_LessonsN02/LessonN02/src/app/Services/message.service.ts
@@ -0,0 +1,20 @@
+﻿import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class MessageService {
+  public messages: string[] = [];
+
+  public addMessage(message: string): void {
+    this.messages.push(message);
+  }
+
+  public clearMessages(): void {
+    this.messages = [];
+  }
+
+  constructor() {
+  }
+}
