6eac0b97431674a1823f224981b7ad505fa0893f
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-05-25 11:00:16 +0500

    Change file structure

---
+++ Angular_LessonsN02/LessonN02/src/app/Components/heroes/heroes.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from "../../Models/hero";
+import {NgForOf, NgIf, UpperCasePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
+import {HeroService} from "../../Services/hero.service";
+import {MessageService} from "../../Services/message.service";
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [
+    UpperCasePipe,
+    FormsModule,
+    NgForOf,
+    NgIf,
+    HeroDetailComponent
+  ],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css',
+  providers: [HeroService]
+})
+export class HeroesComponent implements OnInit {
+
+  public selectedHero?: Hero;
+  public heroes: Hero[] = [];
+  private heroService: HeroService;
+  private messageService: MessageService;
+
+  constructor(heroService: HeroService, messageService: MessageService) {
+    this.messageService = messageService;
+    this.heroService = heroService;
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    this.messageService.addMessage(`HeroesComponent: Selected hero id = ${hero.id}`);
+  }
+
+  private getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+}
