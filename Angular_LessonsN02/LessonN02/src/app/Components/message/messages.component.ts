bc3da1986f959ee996bec86fa5f3a4393658b77a
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-05-25 13:15:57 +0500

    Add a message component

---
+++ Angular_LessonsN02/LessonN02/src/app/Components/message/messages.component.ts
@@ -0,0 +1,20 @@
+﻿import {Component} from '@angular/core';
+import {MessageService} from "../../Services/message.service";
+import {NgForOf, NgIf} from "@angular/common";
+
+@Component({
+    selector: 'app-message',
+    standalone: true,
+  imports: [
+    NgIf,
+    NgForOf,
+  ],
+    templateUrl: './messages.component.html',
+    styleUrl: './messages.component.css'
+})
+
+export class MessagesComponent {
+    constructor(public messageService: MessageService) {
+    }
+}
+
--- Angular_LessonsN02/LessonN02/src/app/app.component.ts
+++ Angular_LessonsN02/LessonN02/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeroesComponent} from "./heroes/heroes.component";
+import {HeroesComponent} from "./Components/heroes/heroes.component";
+import {MessagesComponent} from "./Components/message/messages.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent],
+  imports: [RouterOutlet, HeroesComponent, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
