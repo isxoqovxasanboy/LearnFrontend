7858507678541ab077e77c2586877e0f648b477c
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-05-26 16:49:49 +0500

    Add dashboard component

---
+++ Angular_LessonsN02/LessonN02/src/app/Components/dashboard/dashboard.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from "../../Models/hero";
+import {HeroService} from "../../Services/hero.service";
+import {RouterLink} from "@angular/router";
+import {NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-dashboard',
+  standalone : true,
+  imports: [
+    RouterLink,
+    NgForOf
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ],
+  providers: [HeroService]
+})
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
