3c1d44a23185f9250afda9b15328687f21b65e1c
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-05-26 16:50:47 +0500

    Change ui and logic

--- Angular_LessonsN02/LessonN02/src/app/app.component.ts
+++ Angular_LessonsN02/LessonN02/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {HeroesComponent} from "./Components/heroes/heroes.component";
 import {MessagesComponent} from "./Components/messages/messages.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent, MessagesComponent],
+  imports: [RouterOutlet, HeroesComponent, MessagesComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
