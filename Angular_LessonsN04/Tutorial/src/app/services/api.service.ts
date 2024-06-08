1c8d0c85d16289a862dd838a549386c3b0bb4379
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-06-08 13:10:11 +0500

    Add communication in the backend

--- Angular_LessonsN04/Tutorial/src/app/services/api.service.ts
+++ Angular_LessonsN04/Tutorial/src/app/services/api.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Options} from "../../types";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private httpClient: HttpClient) {
+  }
 
-  constructor() { }
+  // Used to make a GET request to the API
+  get<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
 }
