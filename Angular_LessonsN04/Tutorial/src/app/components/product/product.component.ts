35b9d7bedad4c1fcebe57dd3c2bdab869804d89f
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-06-08 21:57:44 +0500

    Add a product component

---
+++ Angular_LessonsN04/Tutorial/src/app/components/product/product.component.ts
@@ -0,0 +1,24 @@
+﻿import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Product} from "../../../types";
+import {RatingModule} from "primeng/rating";
+import {FormsModule} from "@angular/forms";
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [RatingModule, FormsModule, NgOptimizedImage],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss'
+})
+export class ProductComponent {
+  @Input() product!: Product;
+  @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
+
+  addRating(): void {
+    this.productOutput.emit(this.product);
+  }
+
+
+
+}
--- Angular_LessonsN04/Tutorial/src/app/home/home.component.ts
+++ Angular_LessonsN04/Tutorial/src/app/home/home.component.ts
@@ -1,23 +1,44 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductsService} from "../services/product.service";
+import {Product} from "../../types";
+import {CommonModule} from "@angular/common";
+import {ProductComponent} from "../components/product/product.component";
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    PaginatorModule,
+    ProductComponent,
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
+
 export class HomeComponent implements OnInit {
 
   constructor(private productsService: ProductsService) {
 
   }
 
+  products: Product[] = [];
+
+  onOutputProduct(product: Product) {
+      console.log(product , 'Output product');
+      this.products.push(product);
+  }
+
+
   ngOnInit(): void {
     this.productsService.getProducts('https://localhost:7267/api/product', {page: 1, perPage: 5})
       .subscribe((products) => {
-        console.log(products);
+        this.products = products.items ;
+        console.log(this.products);
       });
+    console.log(this.products);
+
   }
+
 }
