ec85628c5b5a7083b1831a173ffa665cefe5e40f
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-06-09 17:48:10 +0500

    Add pagination function and p-paginator

--- Angular_LessonsN04/Tutorial/src/app/home/home.component.ts
+++ Angular_LessonsN04/Tutorial/src/app/home/home.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ProductsService} from "../services/product.service";
 import {Product} from "../../types";
 import {CommonModule} from "@angular/common";
 import {ProductComponent} from "../components/product/product.component";
-import { PaginatorModule } from 'primeng/paginator';
+import {PaginatorModule, PaginatorState} from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
@@ -17,28 +17,36 @@ import { PaginatorModule } from 'primeng/paginator';
   styleUrl: './home.component.scss'
 })
 
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor(private productsService: ProductsService) {
 
   }
 
   products: Product[] = [];
+  totalRecords: number = 0;
+  rows : number = 12;
 
   onOutputProduct(product: Product) {
-      console.log(product , 'Output product');
-      this.products.push(product);
+    console.log(product, 'Output product');
+    this.products.push(product);
   }
 
+  onPageChange(event:  PaginatorState) {
+    console.log(event, 'Page change');
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number = 0, perPage: number = 12 ) {
+    this.productsService.getProducts('http://localhost:3000/clothes', {page: page, perPage: perPage})
+      .subscribe((product) => {
+        this.products = product.items;
+        this.totalRecords = product.total;
+      })
+  }
 
   ngOnInit(): void {
-    this.productsService.getProducts('https://localhost:7267/api/product', {page: 1, perPage: 5})
-      .subscribe((products) => {
-        this.products = products.items ;
-        console.log(this.products);
-      });
-    console.log(this.products);
-
+    this.fetchProducts(0, this.rows);
   }
 
 }
